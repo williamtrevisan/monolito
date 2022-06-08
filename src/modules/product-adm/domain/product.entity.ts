a2e3bd5ee643b697b03ab9d17e152e12d5abc906
William Trevisan <williamtrev@hotmail.com>
2022-06-08 18:05:54 -0300

    feat: created a product entity

---
+++ src/modules/product-adm/domain/product.entity.ts
@@ -0,0 +1,47 @@
+import { AggregateRoot } from "../../@shared/domain/entity/aggregate-root.interface";
+import { BaseEntity } from "../../@shared/domain/entity/base.entity";
+import { Uuid } from "../../@shared/domain/value-object/uuid.value-object";
+
+type ProductProps = {
+  id?: Uuid;
+  name: string;
+  description: string;
+  purchasePrice: number;
+  stock: number;
+  createdAt?: Date;
+  updatedAt?: Date;
+};
+
+class Product extends BaseEntity implements AggregateRoot {
+  private _name: string;
+  private _description: string;
+  private _purchasePrice: number;
+  private _stock: number;
+
+  constructor(props: ProductProps) {
+    super(props.id);
+
+    this._name = props.name;
+    this._description = props.description;
+    this._purchasePrice = props.purchasePrice;
+    this._stock = props.stock;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get description(): string {
+    return this._description;
+  }
+
+  get purchasePrice(): number {
+    return this._purchasePrice;
+  }
+
+  get stock(): number {
+    return this._stock;
+  }
+}
+
+export { Product };
