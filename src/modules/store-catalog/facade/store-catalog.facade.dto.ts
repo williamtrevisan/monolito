93e316457fb511899a16a17c0277717e13480a27
William Trevisan <williamtrev@hotmail.com>
2022-06-09 13:33:35 -0300

    chore: created input and output dto forStoreCatalogFacadeInterfacee and facade interface

---
+++ src/modules/store-catalog/facade/store-catalog.facade.dto.ts
@@ -0,0 +1,27 @@
+interface FindStoreCatalogFacadeInputDTO {
+  id: string;
+}
+
+interface FindStoreCatalogFacadeOutputDTO {
+  id: string;
+  name: string;
+  description: string;
+  salesPrice: number;
+}
+
+type ProductDTOProps = {
+  id: string;
+  name: string;
+  description: string;
+  salesPrice: number;
+};
+
+interface FindAllStoreCatalogFacadeOutputDTO {
+  products: ProductDTOProps[];
+}
+
+export {
+  FindStoreCatalogFacadeInputDTO,
+  FindStoreCatalogFacadeOutputDTO,
+  FindAllStoreCatalogFacadeOutputDTO,
+};
