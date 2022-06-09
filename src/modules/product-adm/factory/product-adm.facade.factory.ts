fe00ce2d056de1667799c0693aa27dfe6c19d89a
William Trevisan <williamtrev@hotmail.com>
2022-06-09 12:02:39 -0300

    chore: included checkStockUseCase into a facade factory

--- src/modules/product-adm/factory/product-adm.facade.factory.ts
+++ src/modules/product-adm/factory/product-adm.facade.factory.ts
@@ -1,16 +1,18 @@
 import { ProductAdmFacade } from "../facade/product-adm.facade";
 import { ProductRepository } from "../infrastructure/sequelize/repository/product.repository";
 import { AddProductUseCase } from "../usecase/add-product/add-product.usecase";
+import { CheckStockUseCase } from "../usecase/check-stock/check-stock.usecase";
 
 class ProductAdmFacadeFactory {
   static create() {
     const productRepository = new ProductRepository();
 
     const addProductUseCase = new AddProductUseCase(productRepository);
+    const checkStockUseCase = new CheckStockUseCase(productRepository);
 
     return new ProductAdmFacade({
       addProductUseCase,
-      checkStockUseCase: undefined,
+      checkStockUseCase,
     });
   }
 }
