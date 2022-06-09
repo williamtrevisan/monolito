e8cad3b1b71ce85286592407939b9b03c99b0393
William Trevisan <williamtrev@hotmail.com>
2022-06-09 14:39:22 -0300

    feat: created a clientModel

---
+++ src/modules/client-adm/infrastructure/model/client.model.ts
@@ -0,0 +1,28 @@
+import { Column, Model, PrimaryKey, Table } from "sequelize-typescript";
+
+@Table({
+  tableName: "clients",
+  timestamps: false,
+})
+class ClientModel extends Model {
+  @PrimaryKey
+  @Column({ allowNull: false })
+  id: string;
+
+  @Column({ allowNull: false })
+  name: string;
+
+  @Column({ allowNull: false })
+  email: string;
+
+  @Column({ allowNull: false })
+  address: string;
+
+  @Column({ allowNull: false })
+  createdAt: Date;
+
+  @Column({ allowNull: false })
+  updatedAt: Date;
+}
+
+export { ClientModel };
