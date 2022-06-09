ab1b9e099425199064b7d60892ab77b381ab6eeb
William Trevisan <williamtrev@hotmail.com>
2022-06-09 11:56:54 -0300

    test: created test for get stock

---
+++ src/modules/product-adm/usecase/check-stock/check-stock.usecase.spec.ts
@@ -0,0 +1,36 @@
+import { UseCaseInterface } from "../../../@shared/domain/usecase/use-case.interface";
+import { ProductEntity } from "../../domain/product.entity";
+import { ProductGateway } from "../../gateway/product.gateway";
+
+let productRepository: ProductGateway;
+let checkStockUseCase: UseCaseInterface;
+
+describe("CheckStockUseCase unit test", () => {
+  beforeEach(() => {
+    const product = new ProductEntity({
+      name: "Product name",
+      description: "Product description",
+      purchasePrice: 150,
+      stock: 15,
+    });
+    const mockRepository = () => {
+      return {
+        add: jest.fn(),
+        findByPk: jest.fn().mockReturnValue(Promise.resolve(product)),
+      };
+    };
+
+    productRepository = mockRepository();
+    checkStockUseCase = new CheckStockUseCase(productRepository);
+  });
+
+  it("should be able to get a stock of a product", async () => {
+    const input = { productId: "1" };
+
+    const stockOfProduct = await checkStockUseCase.execute(input);
+
+    expect(productRepository.findByPk).toHaveBeenCalled();
+    expect(stockOfProduct.productId).toBe("1");
+    expect(stockOfProduct.stock).toBe(15);
+  });
+});
